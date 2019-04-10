58c4f19e1d139886f393aa44cece69355bebb0d7
Marc Leitner <marc.leitner@htl-salzburg.ac.at>
2019-04-10 13:36:00 +0200

    declarate methode with public void ()

--- ProgBasics-MC_U/Assets/Scripts/ProgBasics.cs
+++ ProgBasics-MC_U/Assets/Scripts/ProgBasics.cs
@@ -2,20 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ProgBasics : MonoBehaviour {
+public class ProgBasics : MonoBehaviour 
+{
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+    {
 
-        Debug.Log("erste Tests mit Operatoren");
+
+        Operators();
+
 
 
     }
-	
-	// Update is called once per frame
-	void Update () {
 
 
+
+    public void Operators()
+    {
+        Debug.Log("erste Tests mit Operatoren");
 
         Debug.Log("Multiplikative Operatoren: * " + (3 * 4));
 
@@ -30,6 +35,47 @@ public class ProgBasics : MonoBehaviour {
 
 
         Debug.Log("Logische Operatoren: & " + (true && true));
+
+    }
+
+
+    // Methoden Deklaration
+    public void XY()
+    {
+        Debug.Log()
+
+    }
+
+
+    public void SWAG()
+    {
+        // Do Something
+    }
+
+    public void Methode()
+    {
+        // Methode do something
+    }
+
+
+
+
+
+
+
+
+    // Update is called once per frame
+    void Update () 
+    {
+
+
+
+
+
+
+
+
+
 
     }
 }
